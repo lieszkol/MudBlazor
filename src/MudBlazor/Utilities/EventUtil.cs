7cebbdeae940ce18fc3841ae7ab01409305aeee4
Laszlo Lieszkovszky <Laszlo.Mana@Lieszkovszky.com>
2025-09-28 11:20:10 +0200

    “Chat” funkcionalitás

--- src/MudBlazor/Utilities/EventUtil.cs
+++ src/MudBlazor/Utilities/EventUtil.cs
@@ -15,6 +15,66 @@ namespace MudBlazor
     /// </summary>
     public static class EventUtil
     {
+        #region FromMudBlazorV8
+        /// <summary>
+        /// Converts the provided <see cref="Action"/> callback into a non-rendering event handler.
+        /// </summary>
+        /// <param name="component">The component that handles exceptions.</param>
+        /// <param name="callback">The action callback to be converted.</param>
+        /// <returns>A non-rendering event handler.</returns>
+        public static Action AsNonRenderingEventHandler(this ComponentBase component, Action callback)
+            => new SyncReceiver(callback).Invoke;
+
+        /// <summary>
+        /// Converts the provided <see cref="Action{TValue}"/> callback into a non-rendering event handler.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the callback argument.</typeparam>
+        /// <param name="callback">The action callback to be converted.</param>
+        /// <param name="component">The component that handles exceptions.</param>
+        /// <returns>A non-rendering event handler.</returns>
+        public static Action<TValue> AsNonRenderingEventHandler<TValue>(this ComponentBase component, Action<TValue> callback)
+            => new SyncReceiver<TValue>(callback).Invoke;
+
+        /// <summary>
+        /// Converts the provided <see cref="Func{Task}"/> callback into a non-rendering event handler.
+        /// </summary>
+        /// <param name="callback">The asynchronous callback to be converted.</param>
+        /// <param name="component">The component that handles exceptions.</param>
+        /// <returns>A non-rendering event handler.</returns>
+        public static Func<Task> AsNonRenderingEventHandler(this ComponentBase component, Func<Task> callback)
+            => new AsyncReceiver(callback).Invoke;
+
+        /// <summary>
+        /// Converts the provided <see cref="Func{TValue, Task}"/> callback into a non-rendering event handler.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the callback argument.</typeparam>
+        /// <param name="callback">The asynchronous callback to be converted.</param>
+        /// <param name="component">The component that handles exceptions.</param>
+        /// <returns>A non-rendering event handler.</returns>
+        public static Func<TValue, Task> AsNonRenderingEventHandler<TValue>(this ComponentBase component, Func<TValue, Task> callback)
+            => new AsyncReceiver<TValue>(callback).Invoke;
+
+        //private record SyncReceiver(ComponentBase component, Action callback) : ReceiverBase()
+        //{
+        //    public void Invoke() => callback();
+        //}
+
+        //private record SyncReceiver<T>(ComponentBase component, Action<T> callback) : ReceiverBase()
+        //{
+        //    public void Invoke(T arg) => callback(arg);
+        //}
+
+        //private record AsyncReceiver(ComponentBase component, Func<Task> callback) : ReceiverBase()
+        //{
+        //    public Task Invoke() => callback();
+        //}
+
+        //private record AsyncReceiver<T>(ComponentBase component, Func<T, Task> callback) : ReceiverBase()
+        //{
+        //    public Task Invoke(T arg) => callback(arg);
+        //}
+        #endregion
+
         /// <summary>
         /// Converts the provided <see cref="Action"/> callback into a non-rendering event handler.
         /// </summary>
